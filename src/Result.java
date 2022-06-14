e7b141e164ee1daff84384acfb2e29d1ff799ab0
aazayka <aazayka@mail.ru>
2022-06-15 00:46:02 +0200

    Easy task with 2 strings and common symbol

--- src/Result.java
+++ src/Result.java
@@ -1,6 +1,31 @@
+import java.util.Arrays;
+
 public class Result {
 
+    public static String twoStrings(String s1, String s2) {
+        // Write your code here
+        byte[] b1 = s1.getBytes();
+        byte[] b2 = s2.getBytes();
+        Arrays.sort(b1);
+        Arrays.sort(b2);
+
+        int i1 = 0;
+        int i2 = 0;
+
+        while(i1 < b1.length && i2 < b2.length) {
+            if (b1[i1] == b2[i2]) {
+                return "YES";
+            } else if (b1[i1] < b2[i2]) {
+                i1++;
+            } else {
+                i2++;
+            }
+        }
+
+        return "NO";
+    }
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(twoStrings("hi", "world"));
     }
 }
